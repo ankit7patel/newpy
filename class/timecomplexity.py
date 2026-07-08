59dab5180948d6552d5aa94cc6245bf3d10476cb
ankit7patel <146925413+ankit7patel@users.noreply.github.com>
2026-07-08 21:30:34 +0530

    Added data

--- class/timecomplexity.py
+++ class/timecomplexity.py
@@ -113,19 +113,161 @@ def fact(n):
 a=fact(n)
 print(a)
 
- """
+
 
 num=[10,20,30,405,500,84]
 
 x=405
 for i in num:
-    if i == x :
-        
+    if i == x :  
         print("found",i) 
         
-    
+
+
+#prefex sum 
+arr = [10, 20, 30, 40]
+
+prefix = []
+total = 0
+
+prefix=[]
+total=0
+
+for num in arr:
+    total = total + num
+    prefix.append(total)
+print(prefix)
 
 
 
 
 
+nums = [5, 12, 50, 25, 10]
+
+maximum = nums[0]
+
+for num in nums :
+    if num > maximum :
+        maximum  =  num
+print(maximum)
+
+
+
+nums = [5, 12, 50, 25, 10]
+total=0 
+
+for i in nums:
+    total+=i
+
+print(total)
+
+
+
+#List me kitni baar ek number aaya hai
+
+nums = [5, 12, 5, 50, 25, 5, 10]
+target=25
+count=0
+for num in nums:
+    if num == target:
+        count += 1
+print(count)
+
+
+#list me number search 
+
+nums=[12,32,54,65,87,91,82,8,82,64,82]
+target=822
+found = False
+
+for num in nums:
+    if num == target:
+        found = True
+        break
+
+
+if found:
+    print("mil gya")
+
+else:
+    print("not")
+
+
+
+#List me Duplicate Numbers
+nums = [10, 20, 30, 20, 40, 10, 50, 30]
+duplicate=[]
+
+for num in nums:
+    if nums.count(num) > 1 and num not in duplicate:
+        duplicate.append(num)
+print(duplicate)
+
+
+
+#Sirf Odd Numbers ki Nayi List Banao
+
+nums=[10,20,30,40,59,39,382,84,39,49,4,7,2,12,512,15,73,81,17,777,72,1,9,29,92,38,8]
+
+odd=[]
+for num in nums:
+    if num%2!=0:
+        odd.append(num)
+
+print(odd)
+
+#Do Lists ke Same Index ke Numbers Add Karo
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+
+result=[]
+
+for i in range(len(list1)):
+
+    result.append(list1[i]+list2[i])
+print(result)
+
+
+
+
+
+nums=list(map(int,input(" enter a list :").split()))
+odd=[]
+
+for i in nums:
+    if i % 2 != 0:
+        odd.append(i)
+
+print(odd)
+
+ """
+
+
+
+#amstrong number 
+
+nums = [10, 15, 22, 31, 40, 55, 61] 
+
+even = []
+sum=0
+for num in nums:
+    if num % 2 == 0:
+        even.append(num)
+        sum+=num
+
+print(even , "sum of number : ", sum , end="\n")
+
+num = int(input("Enter a number: "))
+
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum = sum + digit ** 3
+    temp = temp // 10
+
+if sum == num:
+    print(num, "is an Armstrong Number")
+else:
+    print(num, "is not an Armstrong Number")
